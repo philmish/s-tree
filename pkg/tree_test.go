fcbeea323327610044cac2d3dea279eef5091b19
philmish <thantalosdream@protonmail.com>
2022-09-04 01:07:12 +0200

    feat: add unit test for multiple insertions in tree unit tests

--- pkg/tree_test.go
+++ pkg/tree_test.go
@@ -17,6 +17,24 @@ func TestAddBranch(t *testing.T) {
 	}
 }
 
+func TestMultipleAddBranch(t *testing.T) {
+    tree :=  createTree()
+    data := []values{
+        {[]byte("abc"), []byte("de"),},
+        {[]byte("abc"), []byte("ijk"),},
+    }
+    var err error
+    for _, i := range data {
+        err = tree.addBranch(i)
+        if err != nil {
+            t.Errorf("Failed to add values %v and %v\n", i[0], i[1])
+        }
+    }
+    if tree.Depth() != 2 {
+        t.Errorf("Expected depth 2, got %d\n", tree.Depth())
+    }
+}
+
 func TestSearchNode(t *testing.T) {
     tree := createTree()
     vals := values{[]byte("abc"), []byte("de"),} 
