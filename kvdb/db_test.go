9f20d8727aa23fa4517de0d9d22bf0c92927a2a0
philmish <thantalosdream@protonmail.com>
2022-09-05 22:43:40 +0200

    feat: add unit test for SET command of DB server

--- kvdb/db_test.go
+++ kvdb/db_test.go
@@ -1,6 +1,7 @@
 package kvdb
 
 import (
+	"net"
 	"os"
 	"testing"
 )
@@ -13,4 +14,31 @@ func TestDBStartAndStop(t *testing.T) {
     db.Run()
     go db.serve()
     db.Stop()
+}
+
+func TestDBSet(t *testing.T) {
+	db := NewDB("/tmp/kvdbtest")
+    db.Run()
+    defer db.Stop()
+    go db.serve()
+    conn, err := net.Dial("unix", "/tmp/kvdbtest")
+    defer conn.Close()
+    if err != nil {
+        t.Errorf("Failed to connect to DB")
+    }
+    query := "SET a b"
+    n, err := conn.Write([]byte(query))
+    if err != nil {
+        t.Errorf("Failed to write to DB")
+    }
+    buf := make([]byte, 1024)
+    n, err = conn.Read(buf)
+    if err != nil {
+        t.Errorf("Failed to recieve from DB")
+    }
+    if n == 0 {
+        t.Errorf("No answer recieved")
+    }
+    msg := buf[:n]
+    t.Logf("Msg: %s\n", msg)
 }
