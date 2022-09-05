51e4b1885b70903284064ca1ccdd66c3a95783f9
philmish <thantalosdream@protonmail.com>
2022-09-06 00:09:13 +0200

    refactor: move sending commands to own func

--- kvdb/client.go
+++ kvdb/client.go
@@ -9,29 +9,32 @@ type KvClient struct {
 	Addr string
 }
 
+func (kvc *KvClient) send(query string, conn net.Conn) (string, error) {
+	n, err := conn.Write([]byte(query))
+	if err != nil {
+		return "", err
+	}
+	buf := make([]byte, 256)
+	n, err = conn.Read(buf)
+	if err != nil {
+		return "", err
+	}
+	str := buf[:n]
+    return string(str), nil
+}
+
 func (kvc *KvClient) Ping() error {
 	conn, err := net.Dial("unix", kvc.Addr)
 	if err != nil {
 		return err
 	}
 	defer conn.Close()
-	msg := []byte("PING")
-	n, err := conn.Write(msg)
-	if err != nil {
-		return err
-	}
-	fmt.Println("SEND PONG")
-	if n == 0 {
-		return fmt.Errorf("No data send")
-	}
-	buf := make([]byte, 256)
-	n, err = conn.Read(buf)
-	if err != nil {
-		return err
-	}
-	str := buf[:n]
-	if string(str) != "PONG" {
-		return fmt.Errorf("Expected PONG recieved: %s\n", string(str))
+    result, err := kvc.send("PING", conn) 
+    if err != nil {
+        return err
+    }
+	if result != "PONG" {
+		return fmt.Errorf("Expected PONG recieved: %s\n", result)
 	}
 	fmt.Println("RECIEVED: PONG")
 	return nil
@@ -44,18 +47,12 @@ func (kvc *KvClient) Set(key, val string) error {
 	}
 	defer conn.Close()
 	query := fmt.Sprintf("SET %s %s", key, val)
-	n, err := conn.Write([]byte(query))
-	if err != nil {
-		return err
-	}
-	buf := make([]byte, 256)
-	n, err = conn.Read(buf)
-	if err != nil {
-		return err
-	}
-	str := buf[:n]
-	if string(str) != "RES:SUCCESS" {
-		return fmt.Errorf("Failed to set key/value pair: %s\n", string(str))
+    result, err := kvc.send(query, conn)
+    if err != nil {
+        return err
+    }
+	if result != "RES:SUCCESS" {
+		return fmt.Errorf("Failed to set key/value pair: %s\n", result)
 	}
 	return nil
 }
