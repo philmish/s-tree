d8131e44bbaf0b3c5990caec855396859e16979a
philmish <thantalosdream@protonmail.com>
2022-09-05 23:58:24 +0200

    chore: run code formatter

--- kvdb/client.go
+++ kvdb/client.go
@@ -43,19 +43,19 @@ func (kvc *KvClient) Set(key, val string) error {
 		return err
 	}
 	defer conn.Close()
-    query := fmt.Sprintf("SET %s %s", key, val)
-    n, err := conn.Write([]byte(query))
-    if err != nil {
-        return err
-    }
-    buf := make([]byte, 256)
-    n, err = conn.Read(buf)
-    if err != nil {
-        return err
-    }
-    str := buf[:n]
-    if string(str) != "RES:SUCCESS" {
-        return fmt.Errorf("Failed to set key/value pair: %s\n", string(str))
-    }
-    return nil
+	query := fmt.Sprintf("SET %s %s", key, val)
+	n, err := conn.Write([]byte(query))
+	if err != nil {
+		return err
+	}
+	buf := make([]byte, 256)
+	n, err = conn.Read(buf)
+	if err != nil {
+		return err
+	}
+	str := buf[:n]
+	if string(str) != "RES:SUCCESS" {
+		return fmt.Errorf("Failed to set key/value pair: %s\n", string(str))
+	}
+	return nil
 }
