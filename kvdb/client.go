8a93efd4146960c604837ddd943ad613e5d645f0
philmish <thantalosdream@protonmail.com>
2022-09-05 23:23:37 +0200

    feat: add client with ping command and unit tests

---
+++ kvdb/client.go
@@ -0,0 +1,38 @@
+package kvdb
+
+import (
+	"fmt"
+	"net"
+)
+
+type KvClient struct {
+    Addr string
+}
+
+func (kvc *KvClient) Ping() error {
+    conn, err := net.Dial("unix", kvc.Addr)
+    if err != nil {
+        return err
+    }
+    defer conn.Close()
+    msg := []byte("PING")
+    n, err := conn.Write(msg)
+    if err != nil {
+        return err
+    }
+    fmt.Println("SEND PONG")
+    if n == 0 {
+        return fmt.Errorf("No data send")
+    }
+    buf := make([]byte, 256)
+    n, err = conn.Read(buf)
+    if err != nil {
+        return err
+    }
+    str := buf[:n]
+    if string(str) != "PONG" {
+        return fmt.Errorf("Expected PONG recieved: %s\n", string(str))
+    }
+    fmt.Println("RECIEVED: PONG")
+    return nil
+}
