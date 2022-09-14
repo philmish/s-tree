cae833c825f34494a90f9c22450ced7802e7e711
philmish <thantalosdream@protonmail.com>
2022-09-14 03:02:35 +0200

    feat: add test for Set command

--- kvdb/server_test.go
+++ kvdb/server_test.go
@@ -59,3 +59,45 @@ func TestServerSet(t *testing.T) {
 		t.Errorf("Expected RESULT SUCCESS recieved %s\n", msg)
 	}
 }
+
+func TestServerGet(t *testing.T) {
+	server := NewServer("/tmp/TestKvDB")
+	defer server.Stop()
+	buf := make([]byte, 256)
+	time.Sleep(time.Second * 1)
+	conn, err := net.Dial("unix", "/tmp/TestKvDB")
+	defer conn.Close()
+	if err != nil {
+		t.Errorf("error connecting to server: %s", err.Error())
+	}
+	n, err := conn.Write([]byte("SET a b"))
+	if err != nil {
+		t.Errorf("Failed to write to server")
+	}
+	n, err = conn.Read(buf)
+	if err != nil {
+		t.Errorf("Failed to read response from server: %s", err.Error())
+	}
+	msg := string(buf[:n])
+	if msg != "RESULT SUCCESS" {
+		t.Errorf("Expected RESULT SUCCESS recieved %s\n", msg)
+	}
+	conn2, err := net.Dial("unix", "/tmp/TestKvDB")
+	defer conn2.Close()
+	if err != nil {
+		t.Errorf("error connecting to server for reading: %s", err.Error())
+	}
+	n, err = conn2.Write([]byte("GET a"))
+	if err != nil {
+		t.Errorf("Failed to write to server for Get")
+	}
+	buf = make([]byte, 256)
+	n, err = conn2.Read(buf)
+	if err != nil {
+		t.Errorf("Failed to read from server for Get")
+	}
+	msg = string(buf[:n])
+	if msg != "RESULT b" {
+		t.Errorf("Expected RESULT b, recieved %s", msg)
+	}
+}
