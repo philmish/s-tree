1fd9d961ee547de88e9ae2e78a6bf88e380844f3
philmish <thantalosdream@protonmail.com>
2022-09-21 00:14:42 +0200

    refactor: extract db commands to functions

--- kvdb/commands.go
+++ kvdb/commands.go
@@ -20,39 +20,12 @@ func (c *command) execute(t *pkg.RadixTree) string {
 		if len(c.args) == 0 {
 			return "ERROR Not Enough args"
 		}
-		query := []byte(c.args[0])
-		node, err := t.ThreadSafeSearchNode(query)
-		if err != nil {
-			return fmt.Sprintf("ERROR %s", err.Error())
-		}
-		if len(node.Children) < 1 {
-			return "NOT FOUND"
-		}
-		value := node.Children[0].Value
-		return fmt.Sprintf("RESULT %s", string(value))
+		return getKey(c.args[0], t)
 	case "SET":
 		if len(c.args) < 2 {
 			return "ERROR Not enough args"
 		}
-		key := c.args[0]
-		val := c.args[1]
-		vals := [][]byte{[]byte(key), []byte(val)}
-		node, err := t.ThreadSafeSearchNode([]byte(key))
-		if err != nil {
-			err = t.ThreadSafeAddBranch(vals)
-			if err != nil {
-				return fmt.Sprintf("ERROR %s", err.Error())
-			}
-			return "RESULT SUCCESS"
-		}
-		if len(node.Children) > 0 {
-			node.Children = []*pkg.Node{}
-		}
-		err = t.ThreadSafeRadixAdd(vals)
-		if err != nil {
-			return fmt.Sprintf("ERROR %s", err.Error())
-		}
-		return "RESULT SUCCESS"
+		return addKeyVal(c.args[0], c.args[1], t)
 	default:
 		return "ERROR Unknown command"
 	}
@@ -69,4 +42,36 @@ func parseCommand(data []byte) (*command, error) {
 		cmd.args = parts[1:]
 	}
 	return &cmd, nil
+}
+
+func getKey(key string, t *pkg.RadixTree) string {
+	node, err := t.ThreadSafeSearchNode([]byte(key))
+	if err != nil {
+		return fmt.Sprintf("ERROR %s", err.Error())
+	}
+	if len(node.Children) < 1 {
+		return "NOT FOUND"
+	}
+	value := node.Children[0].Value
+	return fmt.Sprintf("RESULT %s", string(value))
+}
+
+func addKeyVal(key, val string, t *pkg.RadixTree) string {
+	vals := [][]byte{[]byte(key), []byte(val)}
+	node, err := t.ThreadSafeSearchNode([]byte(key))
+	if err != nil {
+		err = t.ThreadSafeAddBranch(vals)
+		if err != nil {
+			return fmt.Sprintf("ERROR %s", err.Error())
+		}
+		return "RESULT SUCCESS"
+	}
+	if len(node.Children) > 0 {
+		node.Children = []*pkg.Node{}
+	}
+	err = t.ThreadSafeRadixAdd(vals)
+	if err != nil {
+		return fmt.Sprintf("ERROR %s", err.Error())
+	}
+	return "RESULT SUCCESS"
 }
