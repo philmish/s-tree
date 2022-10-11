21e31a54ec9450ce6c1989131ddfd35243996c17
philmish <thantalosdream@protonmail.com>
2022-10-11 22:56:50 +0200

    feat: add unit test for IntNode

--- pkg/typed_node_test.go
+++ pkg/typed_node_test.go
@@ -18,3 +18,18 @@ func TestStrNode(t *testing.T) {
 		t.Errorf("Expected %s found %s in value of StrNode", val, ret)
 	}
 }
+
+func TestIntNode(t *testing.T) {
+	val := 123
+	node, err := IntNode(val, nil)
+	if err != nil {
+		t.Errorf("%s\nFailed to create IntNode", err.Error())
+	}
+	ret, err := node.GetValue()
+	if err != nil {
+		t.Errorf("%s\nFailed to read value of IntNode", err.Error())
+	}
+	if ret != val {
+		t.Errorf("Expected %d found %d in value of IntNode", val, ret)
+	}
+}
