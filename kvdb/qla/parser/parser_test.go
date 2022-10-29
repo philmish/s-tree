d0437658973370764e425a0b3c2a5f146a402736
philmish <thantalosdream@protonmail.com>
2022-10-29 18:38:13 +0200

    update: extend unit test for add statement parsing

--- kvdb/qla/parser/parser_test.go
+++ kvdb/qla/parser/parser_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/philmish/s-tree/kvdb/qla"
+	"github.com/philmish/s-tree/kvdb/qla/ast"
 )
 
 func TestAddStatement(t *testing.T) {
@@ -22,4 +23,38 @@ func TestAddStatement(t *testing.T) {
 	if len(programm.Statements) != 2 {
 		t.Fatalf("Expected 2 Statements, got %d", len(programm.Statements))
 	}
+
+	tests := []struct {
+		key string
+		val string
+	}{
+		{key: "Hello", val: "World"},
+		{key: "Foo", val: "Bar"},
+	}
+	for i, tc := range tests {
+		stmt := programm.Statements[i]
+		if !testAddStatement(t, stmt, tc.key, tc.val) {
+			return
+		}
+	}
+}
+
+func testAddStatement(t *testing.T, s ast.Statement, key, val string) bool {
+	if s.TokenLiteral() != "ADD" {
+		t.Errorf("Expected ADD as literal, found %q", s.TokenLiteral())
+		return false
+	}
+
+	addStmt, ok := s.(*ast.AddStatement)
+	if !ok {
+		t.Errorf("statement is not a AddStatement, found %T", s)
+		return false
+	}
+
+	if addStmt.Key != key || addStmt.Value != val {
+		t.Errorf("Expected key value pair %s:%s, found %s:%s", key, val, addStmt.Key, addStmt.Value)
+		return false
+	}
+
+	return true
 }
